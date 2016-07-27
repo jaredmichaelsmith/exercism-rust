5c1b7ff3f2aa4568ed8dc31a2c045d88f0a8d0c9
jaredmichaelsmith <jared@jaredsmith.io>
2016-07-26 20:37:08 -0500

    Update gigasecond to be more expressive

--- gigasecond/src/lib.rs
+++ gigasecond/src/lib.rs
@@ -3,6 +3,5 @@ use chrono::*;
 
 pub fn after(start_date: DateTime<UTC>) -> DateTime<UTC> {
     let ten: i64 = 10;
-    let end_date = start_date + Duration::seconds(ten.pow(9));
-    return end_date;
+    start_date + Duration::seconds(ten.pow(9))
 }
